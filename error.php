2cc5b017941e7d4d60a3655eae5bf845410eaa43
Mohannad Saleh <msaleh@payfort.com>
2018-11-13 13:39:06 +0200

    Update error.php
    remove echo for testing parameters

--- error.php
+++ error.php
@@ -16,34 +16,10 @@
             
             <?php if(isset($_REQUEST['error_msg'])) : ?>
             <section>
-                <div class="error"><?php echo $_REQUEST['error_msg']?></div>
+                <div class="error">error message : error_msg</div>
             </section>
             <div class="h-seperator"></div>
-            <?php endif; ?>
-
-            <?php if(isset($_REQUEST['merchant_reference'])): ?>
-            <section class="details">
-                <h3>Response Details</h3>
-                <br/>
-                <table>
-                    <tr>
-                        <th>
-                            Parameter Name
-                        </th>
-                        <th>
-                            Parameter Value
-                        </th>
-                    </tr>
-                <?php
-                   foreach($_REQUEST as $k => $v) {
-                       echo "<tr>";
-                       echo "<td>$k</td><td>$v</td>";
-                       echo "</tr>";
-                   } 
-                ?>
-                </table>
-            </section>
-            <?php endif; ?>
+            
 
             <div class="h-seperator"></div>
 
