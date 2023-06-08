dc9bd5270373b9e5cc2dd800e13d5277a9497d6a
Ozgen Cetinkaya <cetozgen@amazon.ae>
2023-06-08 16:39:44 +0400

    previos version cleanup

--- template/footer.php
+++
@@ -1,6 +0,0 @@
-        </div>
-        <footer>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia voluptatibus obcaecati eveniet id reiciendis eligendi
-        </footer>
-    </body>
-</html>
